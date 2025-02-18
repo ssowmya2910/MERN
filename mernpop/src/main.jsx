050b7f7a2ae974ce587cf594e5860e31a32ec29a
ssowmya2910 <ssowmya2910@gmail.com>
2025-02-18 23:40:18 +0530

    (18-2-25TASK)

--- mernpop/src/main.jsx
+++ mernpop/src/main.jsx
@@ -2,9 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Home from './Home.jsx'
+import Contact from './Contact.jsx'
+import About from './About.jsx'
+import Projects from './Projects.jsx'
+import Skills from './Skills.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <About/>
+    <Projects/>
+    <Skills/>
+    <Home/>
+    <Contact/>
   </StrictMode>,
 )
