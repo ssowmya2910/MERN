62da3f528c300903239588713592e1e839a265ba
ssowmya2910 <ssowmya2910@gmail.com>
2025-02-20 14:07:09 +0530

    (20-2-25 FN2)

--- mernpop/src/components/functionalCompo/hooks/UseMemo.jsx
+++ mernpop/src/components/functionalCompo/hooks/UseMemo.jsx
@@ -1,9 +1,7 @@
 import React from 'react'
 import { useState,useMemo } from 'react'
 function slowFunction(num){
-   for(var i=0;i<100000000;i++){
-       
-   }
+   for(var i=0;i<1000;i++){}
    return num*2;
 }
 const UseMemo = () => {
@@ -13,6 +11,9 @@ const UseMemo = () => {
         backgroundColor:theme?"red":"green",
         color:theme? "green":"red",
     }
+    var doublingUpaNumber=useMemo(()=>{
+        return slowFunction(number)
+    },[number])
   return (
     <>
     <button onClick={()=>setTheme(!theme)}>Toggle </button>
@@ -20,7 +21,7 @@ const UseMemo = () => {
     Numberbox:{""}
     <input type="number" value={number} onChange={(e)=>setNumber(e.target.value)}/>
     <h2>The number is{number}</h2>   
-    <h2> the num is {slowFunction(number)} </h2>
+    <h2> the num is {doublingUpaNumber} </h2>
     </div>
     </>
 
